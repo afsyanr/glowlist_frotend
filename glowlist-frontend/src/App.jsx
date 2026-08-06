98361f436f498f776a4fd9ac37242280d9bd5c76
afsyanr <afsyanurr@gmail.com>
2026-08-06 09:38:13 +0700

    AddProduk.jsx

---
+++ glowlist-frontend/src/App.jsx
@@ -0,0 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import Home from "./pages/Home";
+import Produk from "./pages/Produk";
+import Kategori from "./pages/Kategori";
+import Tentang from "./pages/Tentang";
+import AddProduk from "./pages/AddProduk";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
+      <Route path="header" element={<Header />} />
+      <Route path="sidebar" element={<Sidebar />} />
+      <Route path="produk" element={<Produk />} />
+      <Route path="kategori" element={<Kategori />} />
+      <Route path="tentang" element={<Tentang />} />
+      <Route path="addproduk" element={<AddProduk />} />
+      </Route>
+    </Routes>
+    </BrowserRouter>
+  );
+}
---
+++ glowlist-frontend/src/pages/Tentang.jsx
@@ -0,0 +1,19 @@
+// Tentang.jsx
+export default function Tentang() {
+    return (
+        <div className="row text-start">
+            <div className="col-10">
+                <h2 className="fw-bold mb-3">Tentang Saya</h2>
+                <h4>Hallo, Perkenalkan saya Ramaa</h4>
+
+                <p>
+                    XII RPL 1
+                </p>
+
+                <p>
+                    Alasan utama menyukai skincare adalah karena kegiatan ini merawat kesehatan kulit, memberi waktu untuk diri sendiri, dan membuat rasa percaya diri meningkat.
+                </p>
+             </div>
+        </div>
+    );
+}
